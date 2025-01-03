bd52a7c8f26943275697e8d127bf42acb211c8a9
Paulo Santana <paulosantana.sa@gmail.com>
2025-01-03 20:36:22 -0300

    feat: implement JWT authentication guard and public route metadata; update controllers to use new guard and metadata

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -8,7 +8,7 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
-import { CryptographyModule } from '../cryptography/cyptography.module'
+import { CryptographyModule } from '../cryptography/cryptography.module'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
